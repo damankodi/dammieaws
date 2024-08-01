a4b3d6871c127b8118381899419024a3cbbe7422
Daman Bhardwaj <daman.bhardwaj@ba.com>
2024-08-01 17:16:45 +0100

    updateed v.1
    Signed-off-by: Daman Bhardwaj <daman.bhardwaj@ba.com>

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_s3_bucket" "s3-auto-yuvika" {
+resource "aws_s3_bucket" "s3autoyuvika" {
   bucket = "s3_auto"
 }
 
-resource "aws_s3_bucket_acl" "s3-auto-yuvika" {
-  bucket = aws_s3_bucket.s3-auto-yuvika.bucket
+resource "aws_s3_bucket_acl" "s3autoyuvika" {
+  bucket = aws_s3_bucket.s3autoyuvika.bucket
   acl    = "public-read"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "bucket_name" {
-  value = aws_s3_bucket.s3-auto-yuvika.bucket
+  value = aws_s3_bucket.s3autoyuvika.bucket
 }
