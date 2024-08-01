41be57bd915aeda5b63614899dd9995144ee1987
Daman Bhardwaj <daman.bhardwaj@ba.com>
2024-08-01 16:14:24 +0100

    s

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_s3_bucket" "dammieauto" {
-  bucket = "dammieauto"
+resource "aws_s3_bucket" "yuvika" {
+  bucket = "s3_auto"
 }
 
-resource "aws_s3_bucket_acl" "dammieauto" {
-  bucket = aws_s3_bucket.dammieauto.bucket
+resource "aws_s3_bucket_acl" "yuvika" {
+  bucket = aws_s3_bucket.s3_auto.bucket
   acl    = "public-read"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "bucket_name" {
-  value = aws_s3_bucket.dammieauto.bucket
+  value = aws_s3_bucket.s3_auto.bucket
 }
