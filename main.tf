f2e765d2c7663cdee8655303e77ded105f353a89
damankodi <damanbhardwaj@me.com>
2024-08-01 16:16:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_s3_bucket" "yuvika" {
+resource "aws_s3_bucket" "s3_auto" {
   bucket = "s3_auto"
 }
 
-resource "aws_s3_bucket_acl" "yuvika" {
+resource "aws_s3_bucket_acl" "s3_auto" {
   bucket = aws_s3_bucket.s3_auto.bucket
   acl    = "public-read"
 }
