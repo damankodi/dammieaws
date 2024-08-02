dff0e1fd64659ba28448908c89873e232ce6431b
Daman Bhardwaj <daman.bhardwaj@ba.com>
2024-08-02 10:22:40 +0100

    Signed-off-by: Daman Bhardwaj <daman.bhardwaj@ba.com>

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_s3_bucket" "s3autoyuvika" {
-  bucket = "s3autoyuvika"
+resource "aws_s3_bucket" "s3autoyuvika1" {
+  bucket = "s3autoyuvika1"
 }
 
 resource "aws_s3_bucket_acl" "s3autoyuvika1" {
