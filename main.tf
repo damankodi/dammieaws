0ab365d3868fe5f612d3540be02ba315bbf7f99c
Daman Bhardwaj <daman.bhardwaj@ba.com>
2024-08-02 10:21:33 +0100

    Signed-off-by: Daman Bhardwaj <daman.bhardwaj@ba.com>

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "s3autoyuvika" {
   bucket = "s3autoyuvika"
 }
 
-resource "aws_s3_bucket_acl" "s3autoyuvika" {
-  bucket = aws_s3_bucket.s3autoyuvika.bucket
+resource "aws_s3_bucket_acl" "s3autoyuvika1" {
+  bucket = aws_s3_bucket.s3autoyuvika1.bucket
   acl    = "public-read"
 }
